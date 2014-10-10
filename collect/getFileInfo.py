c046d3cbdfa2a1b83636d4568d48320f42704695
Stephen <sgowdy@gmail.com>
2014-10-10 10:33:18 +0100

    Also get file sizes and write txt files instead of stdout

--- collect/getFileInfo.py
+++ collect/getFileInfo.py
@@ -2,23 +2,23 @@ import json
 import urllib2,httplib
 import sys
 
+class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
+    def __init__(self, key, cert):
+        urllib2.HTTPSHandler.__init__(self)
+        self.key = key
+        self.cert = cert
+
+    def https_open(self, req):
+        # Rather than pass in a reference to a connection class, we pass in
+        # a reference to a function which, for all intents and purposes,
+        # will behave as a constructor
+        return self.do_open(self.getConnection, req)
+
+    def getConnection(self, host, timeout=300):
+        return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)
+
 
 def getSites( lfn ):
-    class HTTPSClientAuthHandler(urllib2.HTTPSHandler):
-        def __init__(self, key, cert):
-            urllib2.HTTPSHandler.__init__(self)
-            self.key = key
-            self.cert = cert
-
-        def https_open(self, req):
-            # Rather than pass in a reference to a connection class, we pass in
-            # a reference to a function which, for all intents and purposes,
-            # will behave as a constructor
-            return self.do_open(self.getConnection, req)
-
-        def getConnection(self, host, timeout=300):
-            return httplib.HTTPSConnection(host, key_file=self.key, cert_file=self.cert)
-
     #opener = urllib2.build_opener(HTTPSClientAuthHandler('/home/Stephen/.globus/userkey.pem', '/home/Stephen/.globus/usercert.pem') )
 
     returnedStream = urllib2.urlopen( urllib2.Request( "https://cmsweb.cern.ch/phedex/datasvc/json/prod/filereplicas?lfn=%s" % lfn, None, { "Accept" : "application/json" } ) )
@@ -41,6 +41,30 @@ def getSites( lfn ):
     returnedStream.close()
     return theSites.keys()
 
+def getSize( lfn ):
+    #opener = urllib2.build_opener(HTTPSClientAuthHandler('/home/Stephen/.globus/userkey.pem', '/home/Stephen/.globus/usercert.pem') )
+
+    returnedStream = urllib2.urlopen( urllib2.Request( "https://cmsweb.cern.ch/phedex/datasvc/json/prod/data?file=%s" % lfn, None, { "Accept" : "application/json" } ) )
+
+    theDatasets = json.load( returnedStream )['phedex']["dbs"][0]["dataset"]
+    if len( theDatasets ) != 1:
+        print "Expected exactly one dataset."
+        print theDatasets
+        print lfn
+        sys.exit(1)
+    theBlocks = theDatasets[0]["block"]
+    if len( theBlocks ) != 1:
+        print "Expected exactly one block."
+        print theBlocks
+        print lfn
+        sys.exit(1)
+    theFiles = theBlocks[0]["file"]
+    if len( theFiles ) != 1:
+        print "Expected exactly one file."
+        print theFiles
+        print lfn
+    theFile = theFiles[0]
+    return theFile['size']
 
 theJobInfo = open( "input/Jobs.txt" )
 theFiles = {}
@@ -52,12 +76,20 @@ for line in theJobInfo:
         theFiles[ file ] = site
 theJobInfo.close()
 
-print "#LFN              Site"
+
+eventStore = open( "EventStore.txt", "w" )
+filesFile = open( "Files.txt", "w" )
+eventStore.write( "#LFN              Site\n" )
+filesFile.write( "#LFN             Size\n" )
 for file in theFiles.keys():
     if file.startswith("/store/user"):
-        print file, theFiles[file]
+        eventStore.write( "%s %s\n" % ( file, theFiles[file] ) )
+        filesFile.write( "%s %s\n" % ( file, 1000 ) ) # assume 1GB file
     else:
         sites = getSites( file )
+        size = getSize( file )
+        filesFile.write( "%s %s\n" % ( file, size ) )
         for site in sites:
-            print file, site
-
+            eventStore.write( "%s %s\n" % ( file, site ) )
+eventStore.close()
+filesFile.close()
