3348fad06900b72c9b5d1ebe2b4043c9f4cb7250
Stephen Gowdy <sgowdy@gmail.com>
2015-03-12 14:08:10 +0000

    add jobID to dump output

--- python/Job.py
+++ python/Job.py
@@ -106,8 +106,8 @@ class Job:
                                                          self )
 
     def dump( self ):
-        print "Job: %s(%s%%) %ds CPU %ds wall ( %d-%d ) %d%% penalty %ds read" \
-            % ( self.inputData, self.fractionRead, self.cpuTime,
+        print "Job (%d): %s(%s%%) %ds CPU %ds wall ( %d-%d ) %d%% penalty %ds read" \
+            % ( self.jobID, self.inputData, self.fractionRead, self.cpuTime,
                 self.endTime - self.startTime, self.startTime, self.endTime,
                 self.dataReadCPUHit, self.dataReadyTime )
 
